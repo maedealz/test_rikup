490870f807f17ea69bf7dfa4a19c9d22364c2fe4
maedealz <99009974+maedealz@users.noreply.github.com>
2022-02-23 11:00:59 +0330

    کد مسیج باکس

--- git test/Form1.cs
+++ git test/Form1.cs
@@ -16,5 +16,10 @@ namespace git_test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("jjjjjjjjjjjj");
+        }
     }
 }
